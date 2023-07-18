f8e955040fc0a8d951dbcd715ec33d7e9502d1a2
Keshav Gupta <97406076+keshav-guptaa@users.noreply.github.com>
2023-07-18 12:04:15 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
 
-//create a post
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
   try {
@@ -13,7 +12,6 @@ router.post("/", async (req, res) => {
   }
 });
 
-//update a post
 router.put("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -28,7 +26,6 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-//delete a post
 router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -43,7 +40,6 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-//like / dislike a post
 router.put("/:id/like", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -59,7 +55,6 @@ router.put("/:id/like", async (req, res) => {
   }
 });
 
-//get a post
 router.get("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -69,7 +64,6 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-//get timeline posts
 router.get("/timeline/all", async (req, res) => {
   try {
     const currentUser = await User.findById(req.body.userId);
